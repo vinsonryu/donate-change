2963a8d64111233fb00df0345738156a755f02b0
vinsonryu <vinson2346@gmail.com>
2020-09-27 18:02:34 +0530

    added front-end

--- server/src/app.js
+++ server/src/app.js
@@ -3,9 +3,11 @@ require("dotenv").config();
 
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
 const express = require("express");
 const helmet = require("helmet");
 const logger = require("morgan");
+const path = require('path');
 
 require("./db");
 const api = require("./api/index");
@@ -17,15 +19,51 @@ app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 app.use(helmet());
-
+app.use(bodyParser.urlencoded({extended: true}));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
 if (process.env.NODE_ENV === "development") {
   app.use(logger("dev"));
 }
 
 app.get("/", (req, res) => {
-  res.json({
-    message: "Welcome",
-  });
+  res.render("home"); 
+
+});
+   
+app.get("/About", (req, res) => {
+  res.render("About"); 
+
+});
+
+app.get("/signup", (req, res) => {
+  res.render("signup"); 
+});
+
+app.post("/signup", (req, res) => {
+  res.render("signup"); 
+  console.log(req.body)
+});
+
+app.get("/signin", (req, res) => {
+  res.render("signin"); 
+
+});
+
+app.post("/signin", (req, res) => {
+  res.render("signin"); 
+  console.log(req.body)
+});
+
+app.get("/donate", (req, res) => {
+  res.render("donate"); 
+
+});
+
+app.post("/donate", (req, res) => {
+  res.render("donate"); 
+  console.log(req.body)
 });
 
 app.use("/api", api);
--- server/src/db.js
+++ server/src/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 //Connect to MongoDB
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+mongoose.connect("mongodb://localhost:27017/blogDB", { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to Database'))
